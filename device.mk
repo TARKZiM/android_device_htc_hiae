3d0f1fbfba157b7fb9648884d1b735ed394f5ec8
Paul McLean <pmclean@google.com>
2018-12-17 08:14:18 -0700

    hiae: Adding Audio HAL V5: Direction API
    [TARKZIM] Binderize Audio HAL.
    
    Change-Id: I4251b3853d00308dbd988625f8e7466a66b823fa

--- device.mk
+++ device.mk
@@ -68,7 +68,15 @@ PRODUCT_COPY_FILES +=\
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
+    android.hardware.audio@2.0-service \
+    android.hardware.audio@5.0 \
+    android.hardware.audio@5.0-impl \
+    android.hardware.audio.common@5.0 \
+    android.hardware.audio.common@5.0-util \
     android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio.effect@2.0-service \
+    android.hardware.audio.effect@5.0 \
+    android.hardware.audio.effect@5.0-impl \
     audio.a2dp.default \
     audio.primary.msm8952 \
     audio.r_submix.default \
