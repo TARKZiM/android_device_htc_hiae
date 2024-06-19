3eab92b5b3b71ebb37dcd92fa86e7b12500f9d96
Bruno Martins <bgcngm@gmail.com>
2024-06-19 11:24:00 +0530

    hiae: Remove deprecated trust HAL
    Change-Id: Ic5ac79b4ca60b87a97622877b97f806c07cab907

--- device.mk
+++ device.mk
@@ -362,10 +362,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.hiae
 
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.basic
